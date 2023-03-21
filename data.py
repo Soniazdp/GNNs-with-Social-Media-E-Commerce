6c0dfdd62fa9e7d2875b1036203903e757e6b201
Bonnie <bonnieyl.luo@mail.utoronto.ca>
2023-03-20 21:19:13 -0400

    added hashtags

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 # Data Selection
 # Domain: Cosmetics
-hashtage = ['#fentybeauty', '#sephora', '#hudabeauty', '#maccosmetics', '#narscosmetics', '#bobbibrown', '#diormakeup','#charlottetilbury','#makeupforever','#anastasiabeverlyhills','#urbandecay','#tartecosmetics','#katvondbeauty','#benefitcosmetics','#toofaced','#morphebrushes','#maybelline','#nyxcosmetics','#revlon','#lauramercier','#laneige','#lancome','#clinique'] 
+hashtage = ['fentybeauty', 'sephora', 'hudabeauty', 'maccosmetics', 'narscosmetics', 'bobbibrown', 'diormakeup','charlottetilbury','makeupforever','anastasiabeverlyhills','urbandecay','tartecosmetics','katvondbeauty','benefitcosmetics','toofaced','morphebrushes','maybelline','nyxcosmetics','revlon','lauramercier','laneige','lancome','clinique', 'esteelauder', 'colourpop'] 
+
 print(len(hashtage))
