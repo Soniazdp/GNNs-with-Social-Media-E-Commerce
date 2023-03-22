92a45c4d8ffb5e794f9e2548fb0d5c4291a5ae10
Sonia <48974246+Soniazdp@users.noreply.github.com>
2023-03-21 20:04:05 -0400

    Delete data.py
    not used anymore

--- data.py
+++
@@ -1,5 +0,0 @@
-# Data Selection
-# Domain: Cosmetics
-hashtage = ['fentybeauty', 'sephora', 'hudabeauty', 'maccosmetics', 'narscosmetics', 'bobbibrown', 'diormakeup','charlottetilbury','makeupforever','anastasiabeverlyhills','urbandecay','tartecosmetics','katvondbeauty','benefitcosmetics','toofaced','morphebrushes','maybelline','nyxcosmetics','revlon','lauramercier','laneige','lancome','clinique', 'esteelauder', 'colourpop'] 
-
-print(len(hashtage))
